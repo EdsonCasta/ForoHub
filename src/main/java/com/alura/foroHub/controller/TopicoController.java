6825fdfe26037b11d015d25141636214cf1573d0
EdsonCasta <dedsoncasta07@gmail.com>
2025-08-11 15:33:30 -0500

    Show all topics

--- src/main/java/com/alura/foroHub/controller/TopicoController.java
+++ src/main/java/com/alura/foroHub/controller/TopicoController.java
@@ -5,12 +5,14 @@ import com.alura.foroHub.model.Topico;
 import com.alura.foroHub.repository.TopicoRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/topicos")
@@ -39,4 +41,11 @@ public class TopicoController {
 
         return ResponseEntity.ok("Topico creado correctamente");
     }
+
+    @GetMapping
+    public ResponseEntity<List<Topico>> listarTopicos() {
+        List<Topico> topicos = repository.findAll();
+        return ResponseEntity.ok(topicos);
+    }
+
 }
